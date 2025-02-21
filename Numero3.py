a8f746ae110bae04a6d3e44793686f4071d647b2
tarso <154676177+t4rsss@users.noreply.github.com>
2025-02-21 19:53:23 -0300

    Create Numero3.py

---
+++ Numero3.py
@@ -0,0 +1,7 @@
+import math
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+media = (n1 + n2 + n3)/3
+print(media)
